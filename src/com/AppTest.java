c5373cc465003ee1886c830dacc42be2d5477541
Sudhir MK <sudhirsilver@gmail.com>
2018-03-03 13:03:09 +0530

    Added a few more lines

--- src/com/AppTest.java
+++ src/com/AppTest.java
@@ -5,6 +5,7 @@ public class AppTest {
 	public static void main(String[] args) {
 		System.out.println("Hello Sudhir!!");
 		System.out.println("Now this is something...");
+		System.out.println("This change was done in branch1");
 
 	}
 
